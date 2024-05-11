1c2ab95a75367e208f2eabeebec71e78015c32ae
Ruby <ruby0322@ntu.im>
2024-05-11 09:30:27 +0800

    chore: navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -5,10 +5,16 @@ const Navbar = () => {
     <nav className='w-full flex justify-center border-b border-b-foreground/10 h-16'>
       <div className='w-full max-w-4xl flex justify-between items-center p-3 text-sm'>
         <a href='/reminder'>
-          <h2>提醒訊息生成</h2>
+          <h2>提醒訊息實驗室</h2>
         </a>
         <a href='/image'>
-          <h2>錫罐圖片生成</h2>
+          <h2>錫罐圖片實驗室</h2>
+        </a>
+        <a href='/storage'>
+          <h2>（開發中）內容伺服器實驗室</h2>
+        </a>
+        <a href='/database'>
+          <h2>（開發中）資料庫實驗室</h2>
         </a>
         <AuthButton />
       </div>
