283112d2ffd28ceffa79843c5b674ff994304179
Ruby <ruby0322@ntu.im>
2024-05-11 11:53:01 +0800

    feat: add overflow scroll to storage gallery

--- app/storage/page.tsx
+++ app/storage/page.tsx
@@ -5,9 +5,9 @@ import Image from "next/image";
 const StoragePage = async () => {
   const imageFilenames = await listStorageImageUrls();
   return (
-    <div className='flex flex-col items-center gap-8'>
+    <div className='flex flex-col gap-8'>
       <h2>所有生成過的圖片都會被自動儲存至 Supabase Storage！</h2>
-      <div className='grid  grid-cols-2 w-fit gap-8'>
+      <div className='grid grid-cols-2 w-fit h-[40rem] gap-8 overflow-y-scroll'>
         {imageFilenames?.map((url) => (
           <div
             className='w-full h-full flex items-center justify-center'
