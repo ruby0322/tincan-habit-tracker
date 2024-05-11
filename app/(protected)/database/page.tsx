d4dba48bab0d7b959df52bb9f24ea3f980dc1236
Ruby <ruby0322@ntu.im>
2024-05-11 21:23:24 +0800

    feat: fluffy playground

---
+++ app/(protected)/database/page.tsx
@@ -0,0 +1,16 @@
+import { Card } from "@/components/ui/card";
+
+const DatabasePage = () => {
+  return (
+    <div>
+      Database
+      <div className='flex flex-col gap-4'>
+        <Card>1</Card>
+        <Card>2</Card>
+        <Card>3</Card>
+      </div>
+    </div>
+  );
+};
+
+export default DatabasePage;
