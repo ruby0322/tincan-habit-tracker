946cbe0e24c712eb15cc37f364c19502875e4898
fuffy1020 <fuffy1020@gmail.com>
2024-05-16 23:25:11 +0800

    feat: habit backend finished

--- actions/habit.ts
+++ actions/habit.ts
@@ -1,41 +1,111 @@
 "use server";
 
 import { DailyHabit, HabitTable, LightHabit } from "@/type";
+import { createClient } from "@/utils/supabase/server";
 
 const getDailyHabit = async (
-  user_id: string,
+  creator_user_id: string,
   date: string
 ): Promise<DailyHabit[]> => {
-  /* your code should be placed here */
+  const weekDays = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
+  const supabase = createClient();
+  const { data, error } = await supabase
+    .from('habit')
+    .select('*')
+    .eq('creator_user_id', creator_user_id)
+    .filter(
+      `frequency->>${weekDays[(new Date(date).getDay())]}`,
+      'eq',
+      true
+    );
+
+  if(error){
+    console.error("Error fetching daily habits", error);
+    return [];
+  }
+
+  return data
 };
 
-const getLightHabits = async (user_id): Promise<LightHabit[]> => {
-  /* your code should be placed here */
+const getLightHabits = async (creator_user_id: string): Promise<LightHabit[]> => {
+  const supabase = createClient();
+  const { data, error } = await supabase
+    .from('habit')
+    .select('habit_id, title, picture_url')
+    .eq('creator_user_id', creator_user_id);
+  
+  if(error){
+    console.error("Error fetching light habits", error);
+    return [];
+  }
+
+  return data;
 };
 
 const createHabit = async (
-  habit_id: string,
   creator_user_id: string,
-  created_at: Date,
   title: string,
   picture_url: string,
   message: string,
-  lastest_message_update_time: Date,
   num_daily_goal_unit: number,
   daily_goal_unit: string,
   start_date: Date,
   end_date: Date,
   frequency: {}
 ): Promise<boolean> => {
-  /* your code should be placed here */
+  const supabase = createClient();
+  const { data, error } = await supabase
+   .from('habit')
+   .insert([
+    {
+      creator_user_id,
+      title,
+      picture_url,
+      message,
+      num_daily_goal_unit,
+      daily_goal_unit,
+      start_date,
+      end_date,
+      frequency
+    }
+  ]);
+
+  if(error){
+    console.error("Error creating habit", error);
+    return false;
+  }
+
+  return true;
 };
 
 const deleteHabit = async (habit_id: string): Promise<boolean> => {
-  /* your code should be placed here */
+  const supabase = createClient();
+  const { data, error } = await supabase
+    .from('habit')
+    .delete()
+    .eq('habit_id', habit_id);
+
+  if(error){
+    console.error("Error deleting habit", error);
+    return false;
+  }
+
+  return true;
 };
 
-const getAllHabits = async (user_id: string): Promise<HabitTable[]> => {
-  /* your code should be placed here */
+const getAllHabits = async (creator_user_id: string): Promise<HabitTable[]> => {
+  const supabase = createClient();
+  const { data, error } = await supabase
+    .from('habit')
+    .select('*')
+    .eq('creator_user_id', creator_user_id);
+
+  if(error){
+    console.error("Error fetching all habits", error);
+    return [];
+  }
+
+  return data;
 };
 
-export { getDailyHabit, createHabit, deleteHabit, getAllHabits };
+export { getDailyHabit, getLightHabits, createHabit, deleteHabit, getAllHabits };
--- app/playground/page.tsx
+++ app/playground/page.tsx
@@ -1,30 +1,65 @@
 'use server'
 
-import { createClient } from "@/utils/supabase/server";
+import { getDailyHabit, getLightHabits, createHabit, deleteHabit, getAllHabits } from "@/actions/habit";
 
-export default async function PlaygroundPage () {
+export default async function Playground() {
     
-    const supabase = createClient();
-    const { data, error } = await supabase.from('test').select('*')
+    let creator_user_id = "92a75107-0564-4dae-8be0-29665aaccf2b";
+    let date = "2024-05-16";
+    let habitId = "3a16e62a-3d74-44f5-a06b-b86699763ea5"
 
-    console.log()
-    console.log("Test data:", data);
-    console.log("Error:", error);
+    const fetchDailyHabits = async () => {
+        const dailyHabits = await getDailyHabit(creator_user_id, date);
+        console.log("HI:", dailyHabits);
+    };
+    await fetchDailyHabits();
 
+    // const fetchLightHabits = async () => {
+    //     const lightHabits = await getLightHabits(creator_user_id);
+    //     return (lightHabits);
+    // };
+    // await fetchLightHabits();
 
-    if(error) {
-        console.error("Error fetching data", error.message);
-        return <div>Error fetching data</div>;
-    }
+    const handleCreateHabit = async () => {
+        const success = await createHabit(
+          '92a75107-0564-4dae-8be0-29665aaccf2b',
+          'love',
+          'https://picsum.photos/200',
+          'AAAA',
+          69,
+          '12',
+          new Date("2022-03-25"),
+          new Date("2024-05-23"),
+          {
+            "Fri": false,
+            "Mon": true,
+            "Sat": true,
+            "Sun": true,
+            "Thu": false,
+            "Tue": true,
+            "Wed": true
+          },
+        );
+    };
+    await handleCreateHabit();
 
-    return(
+    // const handleDeleteHabit = async () => {
+    //     const success = await deleteHabit(habitId);
+    // };
+    // await handleDeleteHabit();
+
+    // const fetchAllHabits = async () => {
+    //     const allHabits = await getAllHabits(creator_user_id);
+    //     return (allHabits);
+    // };
+    // await fetchAllHabits();
+
+    return (
         <div>
             <h1>Playground</h1>
             <div>
-                {data.map((t, i) => (
-                    <li key={i}>{t.content}</li>
-                ))}
+                <h2>Get Daily Habits</h2>
             </div>
         </div>
-    ) 
+    );
 }
