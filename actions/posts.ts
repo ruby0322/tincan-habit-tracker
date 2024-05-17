bcd81414a7947573e33ed14fa16eb0c887220b61
Ruby <ruby0322@ntu.im>
2024-05-17 11:44:49 +0800

    fix: User server actions

--- actions/posts.ts
+++ actions/posts.ts
@@ -4,11 +4,10 @@ import { PostTable } from "@/type";
 import { createClient } from "@/utils/supabase/server";
 
 const getMyPosts = async (user_id: string): Promise<PostTable[]> => {
-  /* your code should be placed here */
   const supabase = createClient();
   const { data, error } = await supabase
     .from("post")
-    .select("content, created_at, creator_user_id, habit_id, post_id")
+    .select()
     .eq("user_id", user_id);
   if (error) {
     throw new Error(`Error fetching posts: ${error.message}`);
@@ -20,11 +19,10 @@ const getMyPosts = async (user_id: string): Promise<PostTable[]> => {
 };
 
 const getOtherPosts = async (user_id: string): Promise<PostTable[]> => {
-  /* your code should be placed here */
   const supabase = createClient();
   const { data, error } = await supabase
     .from("post")
-    .select("content, created_at, creator_user_id, habit_id, post_id")
+    .select()
     .neq("user_id", user_id);
   if (error) {
     throw new Error(`Error fetching posts: ${error.message}`);
@@ -36,23 +34,17 @@ const getOtherPosts = async (user_id: string): Promise<PostTable[]> => {
 };
 
 const createPost = async (
-  post_id: string,
   creator_user_id: string,
   habit_id: string,
-  post_time: string,
   content: string
 ): Promise<boolean> => {
-  /* your code should be placed here */
   const supabase = createClient();
-  const { error } = await supabase
-    .from("post")
-    .insert({
-      post_id: post_id,
-      creator_user_id: creator_user_id,
-      habit_id: habit_id,
-      post_time: post_time,
-      content: content,
-    });
+  const { error } = await supabase.from("post").insert({
+    creator_user_id: creator_user_id,
+    habit_id: habit_id,
+    post_time: new Date(),
+    content: content,
+  });
   if (error) {
     throw new Error(`Error creating post: ${error.message}`);
   }
@@ -60,7 +52,6 @@ const createPost = async (
 };
 
 const deletePost = async (post_id: string): Promise<boolean> => {
-  /* your code should be placed here */
   const supabase = createClient();
   const { error } = await supabase.from("post").delete().eq("post_id", post_id);
   if (error) {
