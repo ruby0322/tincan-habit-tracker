b1453f1a3f688827f036a5b916ef00b545569b92
Ruby <ruby0322@ntu.im>
2024-05-17 11:25:21 +0800

    fix: remove redundant imports to resolve build error

--- actions/posts.ts
+++ actions/posts.ts
@@ -2,38 +2,37 @@
 
 import { PostTable } from "@/type";
 import { createClient } from "@/utils/supabase/server";
-import { post } from "cypress/types/jquery";
 
 const getMyPosts = async (user_id: string): Promise<PostTable[]> => {
-    /* your code should be placed here */
-    const supabase = createClient();
-    const { data, error } = await supabase
-        .from('post')
-        .select('content, created_at, creator_user_id, habit_id, post_id')
-        .eq('user_id', user_id)
-    if (error) {
-        throw new Error(`Error fetching posts: ${error.message}`);
-    }
-    if (!data || data.length === 0) {
-        return [];
-    }
-    return data;
+  /* your code should be placed here */
+  const supabase = createClient();
+  const { data, error } = await supabase
+    .from("post")
+    .select("content, created_at, creator_user_id, habit_id, post_id")
+    .eq("user_id", user_id);
+  if (error) {
+    throw new Error(`Error fetching posts: ${error.message}`);
+  }
+  if (!data || data.length === 0) {
+    return [];
+  }
+  return data;
 };
 
 const getOtherPosts = async (user_id: string): Promise<PostTable[]> => {
-    /* your code should be placed here */
-    const supabase = createClient();
-    const { data, error } = await supabase
-        .from('post')
-        .select('content, created_at, creator_user_id, habit_id, post_id')
-        .neq('user_id', user_id)
-    if (error) {
-        throw new Error(`Error fetching posts: ${error.message}`);
-    }
-    if (!data || data.length === 0) {
-        return [];
-    }
-    return data;
+  /* your code should be placed here */
+  const supabase = createClient();
+  const { data, error } = await supabase
+    .from("post")
+    .select("content, created_at, creator_user_id, habit_id, post_id")
+    .neq("user_id", user_id);
+  if (error) {
+    throw new Error(`Error fetching posts: ${error.message}`);
+  }
+  if (!data || data.length === 0) {
+    return [];
+  }
+  return data;
 };
 
 const createPost = async (
@@ -44,27 +43,30 @@ const createPost = async (
   content: string
 ): Promise<boolean> => {
   /* your code should be placed here */
-    const supabase = createClient();
-    const { error } = await supabase
-        .from('post')
-        .insert({ post_id: post_id, creator_user_id: creator_user_id, habit_id: habit_id,  post_time: post_time, content: content})
-    if (error) {
-        throw new Error(`Error creating post: ${error.message}`);
-    }
-    return true;
+  const supabase = createClient();
+  const { error } = await supabase
+    .from("post")
+    .insert({
+      post_id: post_id,
+      creator_user_id: creator_user_id,
+      habit_id: habit_id,
+      post_time: post_time,
+      content: content,
+    });
+  if (error) {
+    throw new Error(`Error creating post: ${error.message}`);
+  }
+  return true;
 };
 
 const deletePost = async (post_id: string): Promise<boolean> => {
   /* your code should be placed here */
-    const supabase = createClient();
-    const { error } = await supabase
-        .from('post')
-        .delete()
-        .eq('post_id', post_id)
-    if (error) {
-        throw new Error(`Error deleting post: ${error.message}`);
-    }
-    return true;
+  const supabase = createClient();
+  const { error } = await supabase.from("post").delete().eq("post_id", post_id);
+  if (error) {
+    throw new Error(`Error deleting post: ${error.message}`);
+  }
+  return true;
 };
 
-export { getMyPosts, getOtherPosts, createPost, deletePost };
+export { createPost, deletePost, getMyPosts, getOtherPosts };
