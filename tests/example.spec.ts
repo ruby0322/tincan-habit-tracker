877ae0de1a835c9eef45c4b328d69743c27ef754
Ruby <ruby0322@ntu.im>
2024-05-21 00:33:53 +0800

    feat: add example test and yarn test command

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,6 +1,6 @@
 import { expect, test } from "@playwright/test";
 
 test("1. Redirect Unauthenticated User to Login Page", async ({ page }) => {
-  await page.goto("https://localhost:3000/manage");
-  await expect(page).toHaveURL("https://localhost:3000/login");
+  await page.goto("http://localhost:3000/manage");
+  await expect(page).toHaveURL("http://localhost:3000/login");
 });
