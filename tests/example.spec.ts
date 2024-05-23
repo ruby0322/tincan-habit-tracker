3590ae308ba0c5ceb01801d9b8f98f3782c0b315
Ruby <ruby0322@ntu.im>
2024-05-23 10:54:20 +0800

    fix: yarn test on localhost with github actions

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -4,6 +4,12 @@ export default defineConfig({
   expect: {
     timeout: 5000,
   },
+  webServer: {
+    timeout: 120000,
+    command: "yarn dev",
+    url: "http://localhost:3000",
+    reuseExistingServer: !process.env.CI,
+  },
 });
 
 test("1. Redirect Unauthenticated User to Login Page", async ({ page }) => {
