237bdc6a7b94d275a778a95d1a363f1e59ded8e8
Ruby <ruby0322@ntu.im>
2024-05-23 10:59:59 +0800

    fix: yarn test on local server

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,18 +1,6 @@
-import { defineConfig, expect, test } from "@playwright/test";
-
-export default defineConfig({
-  expect: {
-    timeout: 5000,
-  },
-  webServer: {
-    timeout: 120000,
-    command: "yarn dev",
-    url: "http://localhost:3000",
-    reuseExistingServer: !process.env.CI,
-  },
-});
+import { expect, test } from "@playwright/test";
 
 test("1. Redirect Unauthenticated User to Login Page", async ({ page }) => {
-  await page.goto("http://localhost:3000/manage");
-  await expect(page).toHaveURL("http://localhost:3000/login");
+  await page.goto("/manage");
+  await expect(page).toHaveURL("/login");
 });
