6b8d2ccc678d0259e1b980ee450a6d07803c4c83
fuffy1020 <fuffy1020@gmail.com>
2024-05-23 18:27:55 +0800

    feat: finish monthly report

--- actions/report.ts
+++ actions/report.ts
@@ -3,7 +3,7 @@
 import { HabitReport, HabitTable, RecordTable } from "@/type";
 import { createClient } from "@/utils/supabase/server";
 
-const getReport = async (habit_id: string): Promise<HabitReport> => {
+const getFullReport = async (habit_id: string): Promise<HabitReport> => {
     const supabase = createClient();
     
     const { data: habitData, error: habitError } = await supabase
@@ -31,7 +31,7 @@ const getReport = async (habit_id: string): Promise<HabitReport> => {
     }
 
     const records: { [key: string]: number } = (recordsData as RecordTable[]).reduce((acc, record) => {
-        const date = new Date(record.created_at).toISOString().split('T')[0]; // Format date as YYYY-MM-DD
+        const date = new Date(record.created_at).toISOString().split('T')[0];
         acc[date] = record.num_completed_unit;
         return acc;
     }, {} as { [key: string]: number });
@@ -44,4 +44,53 @@ const getReport = async (habit_id: string): Promise<HabitReport> => {
     return habitReport;
 };
 
-export { getReport };
+const getMonthlyReport = async (
+    habit_id: string,
+    target_year: number,
+    target_month: number
+) : Promise<HabitReport> => {
+    const supabase = createClient();
+    
+    const startDate = new Date(target_year, target_month - 1, 1).toISOString();
+    const endDate = new Date(target_year, target_month, 0, 23, 59, 59, 999).toISOString();
+    
+    const { data: habitData, error: habitError } = await supabase
+       .from('habit')
+       .select('*')
+       .eq('habit_id', habit_id)
+       .single();
+    
+    if(habitError){
+        console.error("Error fetching habit", habitError);
+    }
+
+    if(!habitData){
+        console.error("Habit data not found");
+    }
+
+    const { data: recordsData, error: recordsError } = await supabase
+       .from('record')
+       .select('created_at, num_completed_unit')
+       .gte('created_at', startDate)
+       .lte('created_at', endDate)
+       .eq('habit_id', habit_id);
+    
+    if(recordsError){
+        console.error("Error fetching records data", recordsError);
+    }
+
+    const records: { [key: string]: number } = (recordsData as RecordTable[]).reduce((acc, record) => {
+        const date = new Date(record.created_at).toISOString().split('T')[0];
+        acc[date] = record.num_completed_unit;
+        return acc;
+    }, {} as { [key: string]: number });
+
+    const monthlyReport: HabitReport = {
+        ...habitData,
+        records,
+    }
+
+    return monthlyReport;
+}
+
+export { getFullReport, getMonthlyReport };
