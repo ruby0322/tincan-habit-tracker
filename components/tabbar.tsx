66c7ca1e247560bf451812e482a1396e31e28d3a
Ruby <ruby0322@ntu.im>
2024-05-26 21:44:25 +0800

    fix: tabbar profile redirect

--- components/tabbar.tsx
+++ components/tabbar.tsx
@@ -1,3 +1,4 @@
+import { createClient } from "@/utils/supabase/server";
 import {
   AlarmClock,
   AreaChart,
@@ -8,6 +9,10 @@ import {
 import Link from "next/link";
 
 const Tabbar = async () => {
+  const supabase = createClient();
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
   return (
     <div className='sm:w-[28rem] w-full fixed sm:bottom-[2vh] bottom-0 shadow-[rgba(50,_50,_105,_0.15)_0px_2px_5px_0px,_rgba(0,_0,_0,_0.05)_0px_1px_1px_0px] z-50 flex w-full items-center justify-center bg-white shadow-lg'>
       <div className='flex h-[4.5rem] w-full items-center gap-2 justify-between px-12'>
@@ -27,7 +32,10 @@ const Tabbar = async () => {
         <Link href='/report' className='text-gray-400 text-lg text-center'>
           <AreaChart />
         </Link>
-        <Link href='/profile' className='text-gray-400 text-lg text-center'>
+        <Link
+          href={`/profile/${user?.id}`}
+          className='text-gray-400 text-lg text-center'
+        >
           <User />
         </Link>
       </div>
